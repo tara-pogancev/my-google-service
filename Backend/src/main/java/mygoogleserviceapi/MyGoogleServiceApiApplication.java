e02c6c3fd3320e6b20690b3bc6749eda22e553b1
Tara Pogančev <tarapogancev@gmail.com>
2022-02-04 22:46:03 +0100

    feat: initial spring application

---
+++ Backend/src/main/java/mygoogleserviceapi/MyGoogleServiceApiApplication.java
@@ -0,0 +1,13 @@
+package mygoogleserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGoogleServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGoogleServiceApiApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/mygoogleserviceapi/MyGoogleServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package mygoogleserviceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyGoogleServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
