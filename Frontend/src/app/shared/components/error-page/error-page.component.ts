27650ccfb7b1bbaab6034e558764345e28e35515
Tara Pogančev <tarapogancev@gmail.com>
2022-02-10 01:42:20 +0100

    feat: Error page redesign

--- Frontend/src/app/shared/components/error-page/error-page.component.ts
+++ Frontend/src/app/shared/components/error-page/error-page.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'error-page',
   templateUrl: './error-page.component.html',
-  styleUrls: ['./error-page.component.scss']
+  styleUrls: ['./error-page.component.scss'],
 })
 export class ErrorPageComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  redirectHomepage() {
+    window.location.href = '/';
   }
-
 }
