1b9db1a49365dd9abca95f197cce2ecc0883fa8d
Tara Pogančev <tarapogancev@gmail.com>
2022-02-10 01:20:47 +0100

    feat: user registration

--- Backend/src/main/java/mygoogleserviceapi/shared/controller/RegistrationController.java
+++ Backend/src/main/java/mygoogleserviceapi/shared/controller/RegistrationController.java
@@ -21,7 +21,7 @@ public class RegistrationController {
     @PostMapping
     public ResponseEntity<?> registerNewUser(@RequestBody UserDTO newUser) {
         ApplicationUser createdUser = userService.registerNewUser(newUser);
-        if (createdUser != null) return ResponseEntity.ok(createdUser.getFullName());
+        if (createdUser != null) return new ResponseEntity(createdUser.getFullName(), HttpStatus.OK);
         else return new ResponseEntity(HttpStatus.BAD_REQUEST);
     }
 
