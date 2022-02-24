11a0878f9d77b7a46a807e19ee3cbc773c7a5d10
Tara Pogančev <tarapogancev@gmail.com>
2022-02-24 14:40:36 +0100

    feat: contacts header

--- Frontend/src/app/google-contacts/components/contacts-header/contacts-header.component.ts
+++ Frontend/src/app/google-contacts/components/contacts-header/contacts-header.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/shared/model/user-model';
+import { AuthService } from 'src/app/shared/services/auth.service';
+import { UserService } from 'src/app/shared/services/user-service';
 
 @Component({
   selector: 'contacts-header',
   templateUrl: './contacts-header.component.html',
-  styleUrls: ['./contacts-header.component.scss']
+  styleUrls: ['./contacts-header.component.scss'],
 })
 export class ContactsHeaderComponent implements OnInit {
+  user: User = new User();
+  searchValue: string = '';
 
-  constructor() { }
+  constructor(
+    private userService: UserService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
+    this.user.id = this.authService.getCurrentUser().id;
+    this.userService.getCurrentUser().subscribe((data) => (this.user = data));
   }
 
+  redirectContacts() {
+    window.location.href = '/contacts';
+  }
+
+  redirectPhotos() {
+    window.location.href = '/photos';
+  }
+
+  redirectEditProfile() {
+    window.location.href = '/edit-profile';
+  }
+
+  signOut() {
+    this.authService.logout();
+  }
+
+  search() {}
+
+  toggleSidebar() {}
+
+  
 }
