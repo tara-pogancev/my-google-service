23941ebce4bf81c90a6a7fa6f04c83afdfc0bf39
Tara Pogančev <tarapogancev@gmail.com>
2022-02-25 11:05:11 +0100

    feat: frontend sidebar

--- Frontend/src/app/google-contacts/components/contacts-sidebar/contacts-sidebar.component.ts
+++ Frontend/src/app/google-contacts/components/contacts-sidebar/contacts-sidebar.component.ts
@@ -3,13 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'contacts-sidebar',
   templateUrl: './contacts-sidebar.component.html',
-  styleUrls: ['./contacts-sidebar.component.scss']
+  styleUrls: ['./contacts-sidebar.component.scss'],
 })
 export class ContactsSidebarComponent implements OnInit {
+  tab: number = 1;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
