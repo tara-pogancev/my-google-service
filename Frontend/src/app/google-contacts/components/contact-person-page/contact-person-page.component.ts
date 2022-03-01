8a79793f7e0c507b8fd3832b4dea3f54ae90ae06
Tara Pogančev <tarapogancev@gmail.com>
2022-03-01 17:48:37 +0100

    fix: reset search on entering new tabs

--- Frontend/src/app/google-contacts/components/contact-person-page/contact-person-page.component.ts
+++ Frontend/src/app/google-contacts/components/contact-person-page/contact-person-page.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchContactsService } from '../contacts-header/search-contacts.service';
 
 @Component({
   selector: 'contact-person-page',
   templateUrl: './contact-person-page.component.html',
-  styleUrls: ['./contact-person-page.component.scss']
+  styleUrls: ['./contact-person-page.component.scss'],
 })
 export class ContactPersonPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private searchContactsService: SearchContactsService) {
+    this.searchContactsService.announceSearchReset();
   }
 
+  ngOnInit(): void {}
 }
--- Frontend/src/app/google-contacts/components/create-new-page/create-new-page.component.ts
+++ Frontend/src/app/google-contacts/components/create-new-page/create-new-page.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { SearchContactsService } from '../contacts-header/search-contacts.service';
 
 @Component({
   selector: 'create-new-page',
   templateUrl: './create-new-page.component.html',
-  styleUrls: ['./create-new-page.component.scss']
+  styleUrls: ['./create-new-page.component.scss'],
 })
 export class CreateNewPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private searchContactsService: SearchContactsService) {
+    this.searchContactsService.announceSearchReset();
   }
 
+  ngOnInit(): void {}
+
+  //TODO add new contact
 }
