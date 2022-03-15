adc549bd6957e4636dc7ac8b4802a9c2043880e4
Tara Pogančev <tarapogancev@gmail.com>
2022-03-15 20:33:46 +0100

    feat: sending files to backend

--- Backend/src/main/java/mygoogleserviceapi/contacts/controller/ImportContactsController.java
+++ Backend/src/main/java/mygoogleserviceapi/contacts/controller/ImportContactsController.java
@@ -2,7 +2,6 @@ package mygoogleserviceapi.contacts.controller;
 
 import lombok.RequiredArgsConstructor;
 import mygoogleserviceapi.contacts.service.interfaces.ImportContactsService;
-import mygoogleserviceapi.contacts.validator.annotation.ValidContactPicture;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,13 +17,14 @@ public class ImportContactsController {
     private final ImportContactsService importContactsService;
 
     @PostMapping
-    public ResponseEntity<?> importFromFile(@RequestPart("file") @ValidContactPicture MultipartFile file, @RequestHeader(name = "Authorization") String jwt) throws IOException {
+    public ResponseEntity<?> importFromFile(@RequestPart("file") MultipartFile file, @RequestHeader(name = "Authorization") String jwt) throws IOException {
+        System.out.println(file.getContentType());
         if (file.getContentType().equals("text/csv")) {
             importContactsService.importFromCSV(file, jwt);
-        } else if (file.getContentType().equals("text/json")) {
+        } else if (file.getContentType().equals("application/json")) {
             importContactsService.importFromJSON(file, jwt);
         } else {
-            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+            return new ResponseEntity<>(null, HttpStatus.I_AM_A_TEAPOT);
         }
 
         return new ResponseEntity<>(null, HttpStatus.OK);
