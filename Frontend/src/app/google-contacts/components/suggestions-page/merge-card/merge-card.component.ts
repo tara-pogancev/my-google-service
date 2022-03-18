dccd49cb4ddc6e1ef8ef99448b9c4083b9bda5f9
Tara Pogančev <tarapogancev@gmail.com>
2022-03-18 23:51:22 +0100

    feat: merge contacts from list

--- Frontend/src/app/google-contacts/components/suggestions-page/merge-card/merge-card.component.ts
+++ Frontend/src/app/google-contacts/components/suggestions-page/merge-card/merge-card.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Contact } from 'src/app/google-contacts/model/contact';
+import { MergeService } from 'src/app/google-contacts/services/merge.service';
+import { RefreshContactsCountService } from '../../contacts-sidebar/refresh-contact-count.service';
 
 @Component({
   selector: 'merge-card',
@@ -8,10 +10,25 @@ import { Contact } from 'src/app/google-contacts/model/contact';
 })
 export class MergeCardComponent implements OnInit {
   @Input() contacts: Contact[] = [];
+  @Output() doRemoveSuggestion: EventEmitter<Contact[]> = new EventEmitter();
 
-  constructor() {}
+  constructor(private mergeService: MergeService, 
+    private refreshContactsCountService: RefreshContactsCountService) {}
 
   ngOnInit(): void {}
 
-  merge() {}
+  excractIds() {
+    var ids = [];
+    for (let contact of this.contacts) {
+      ids.push(contact.id);
+    }
+    return ids;
+  }
+
+  merge() {
+    this.mergeService.mergeContacts(this.excractIds()).subscribe((data) => {
+      this.refreshContactsCountService.announceRefreshing();
+      this.doRemoveSuggestion.emit(this.contacts);
+    });
+  }
 }
--- Frontend/src/app/google-contacts/services/suggestions.service.ts
+++ Frontend/src/app/google-contacts/services/suggestions.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { server } from 'src/app/app-global';
+import { AuthService } from 'src/app/shared/services/auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SuggestionsService {
+  url = server + 'suggestions';
 
-  constructor() { }
+  constructor(private _http: HttpClient, private authService: AuthService) {}
+
+  getSuggestions() {
+    const headers = this.authService.getHeaders();
+    return this._http.get<any>(this.url, {
+      headers: headers,
+    });
+  }
 }
