bc2468a5471843cd4e95bb163e4b37c58120ad44
Tara Pogančev <tarapogancev@gmail.com>
2022-03-19 15:09:33 +0100

    feat: adaptive sidebar

--- Frontend/src/app/google-contacts/components/contacts-page/contacts-page.component.ts
+++ Frontend/src/app/google-contacts/components/contacts-page/contacts-page.component.ts
@@ -1,14 +1,25 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  HostListener,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
 
 @Component({
   selector: 'contacts-page',
   templateUrl: './contacts-page.component.html',
   styleUrls: ['./contacts-page.component.scss'],
 })
-export class ContactsPageComponent implements OnInit {
+export class ContactsPageComponent implements OnInit, AfterViewInit {
   innerWidth: any;
+  @ViewChild(MatDrawer) sidenav!: MatDrawer;
 
   constructor() {}
+  ngAfterViewInit(): void {
+    this.setupSidenav();
+  }
 
   ngOnInit() {
     this.innerWidth = window.innerWidth;
@@ -20,6 +31,27 @@ export class ContactsPageComponent implements OnInit {
 
   @HostListener('window:resize', ['$event'])
   onResize(event: any) {
+    var oldWidth = this.innerWidth;
     this.innerWidth = window.innerWidth;
+
+    if (oldWidth > 768 && this.innerWidth <= 768) {
+      this.setupSidenav();
+    } else if (oldWidth < 768 && this.innerWidth >= 768) {
+      this.setupSidenav();
+    }
+  }
+
+  setupSidenav() {
+    if (this.innerWidth > 768) {
+      this.sidenav.mode = 'side';
+      this.sidenav.opened = true;
+    } else {
+      this.sidenav.mode = 'over';
+      this.sidenav.opened = false;
+    }
+  }
+
+  toggleSidenav() {
+    this.sidenav.toggle();
   }
 }
