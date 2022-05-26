a81a502147b42d9ea990edb8f3590b61ceea5c82
vladimirr9 <74308447+vladimirr9@users.noreply.github.com>
2022-05-26 16:06:20 +0200

    feat: add initial sidebar and header

--- Frontend/src/app/google-photos/photos-page/photos-page.component.ts
+++ Frontend/src/app/google-photos/photos-page/photos-page.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { MatDrawer } from '@angular/material/sidenav';
 import { DomSanitizer } from '@angular/platform-browser';
 import { ProfilePictureService } from 'src/app/shared/services/profile-picture.service';
 
@@ -10,12 +11,46 @@ import { ProfilePictureService } from 'src/app/shared/services/profile-picture.s
 })
 export class PhotosPageComponent implements OnInit {
 
+  innerWidth: any;
+  @ViewChild(MatDrawer) sidenav!: MatDrawer;
 
-  url: any
-  constructor() { }
-  ngOnInit(): void {
-
+  constructor() {}
+  ngAfterViewInit(): void {
+    this.setupSidenav();
   }
 
+  ngOnInit() {
+    this.innerWidth = window.innerWidth;
+  }
+
+  search(searchString: string) {
+    alert(searchString);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    var oldWidth = this.innerWidth;
+    this.innerWidth = window.innerWidth;
+
+    if (oldWidth > 768 && this.innerWidth <= 768) {
+      this.setupSidenav();
+    } else if (oldWidth < 768 && this.innerWidth >= 768) {
+      this.setupSidenav();
+    }
+  }
+
+  setupSidenav() {
+    if (this.innerWidth > 768) {
+      this.sidenav.mode = 'side';
+      this.sidenav.opened = true;
+    } else {
+      this.sidenav.mode = 'over';
+      this.sidenav.opened = false;
+    }
+  }
+
+  toggleSidenav() {
+    this.sidenav.toggle();
+  }
 
 }
